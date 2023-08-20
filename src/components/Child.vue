36ad96d2538ecdd6825077497753cfebe03c14ed
sarker24 <sarker24@gmail.com>
2023-08-20 12:25:00 +0200

    Initial commit

---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import {inject} from 'vue'
+
+const data = inject('myData')
+
+</script>
+
+<template>
+  <h1> {{ data.message }} comes from parent component</h1>
+</template>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {reactive, provide} from 'vue'
+import Child from './Child.vue'
+const data = reactive({
+message: 'Hello World'
+})
+
+provide('myData', data)
+
+</script>
+
+<template>
+  <div>
+    <h1>This is parent component</h1>
+    <Child />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
