e78431ffb90f23ff5bbb9409eb3d67838c4fd959
sarker24 <sarker24@gmail.com>
2023-08-20 15:11:35 +0200

    Add message emeit

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,11 +1,24 @@
 <script setup>
-
+import {ref} from 'vue'
 defineProps(['data'])
+
+const emit = defineEmits();
+
+
+const messageEmitted = ()=>{
+
+const eventData = 'pass data from chile to parent'  
+emit('myData', eventData)
+
+}
 
 </script>
 
 <template>
   <h1> {{ data.message }} comes from parent component</h1>
+  <button @click="messageEmitted" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Click me
+</button>
 </template>
 
 
--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,19 +1,24 @@
 <script setup>
-import {reactive, provide} from 'vue'
+import {reactive, ref} from 'vue'
 import ChildComponent from './ChildComponent.vue'
 const data = reactive({
 message: 'Hello World'
 })
 
-//provide('myData', data)
+const getData = ref('')
+
+const handleEvent = (message)=>{
+  getData.value = message
+}
 
 </script>
 
 <template>
   <div>
     <h1>This is parent component</h1>
-    <ChildComponent :data="data" />
-  </div>
+    <ChildComponent :data="data" @myData = "handleEvent" />
+    <p>Emitted message from ChildComponent: {{ getData }}</p>
+    </div>
 </template>
 
 <style lang="scss" scoped>
